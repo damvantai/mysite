e6365aab559df568955251c281a511bda4869b92
damvantai <tai94bn@gmail.com>
2017-08-10 22:17:16 +0700

    update test polls application

--- polls/tests.py
+++ polls/tests.py
@@ -8,7 +8,7 @@ from django.test import TestCase
 from .models import Question
 
 class QuestionModelTests(TestCase):
-
+        # test github for sublime
     def test_was_published_recently_with_future_question(self):
         """
         was_published_recently() returns False for question whose pub_date is in the future
